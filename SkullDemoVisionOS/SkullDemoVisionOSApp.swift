14e7021519b83756dd7c3b72d7d1e0b5be86ce42
Mohammed Azeem Azeez <mohammed.azeem.azeez@macys.com>
2023-08-13 10:56:31 +0530

    Initial Commit

---
+++ SkullDemoVisionOS/SkullDemoVisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkullDemoVisionOSApp.swift
+//  SkullDemoVisionOS
+//
+//  Created by mohammedazeem.azeez on 13/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SkullDemoVisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
